e2a6556b5997058220e1608e1d121e0a2f44d138
Lstudys <Lstudys@163.com>
2021-11-17 11:14:06 +0800

    设置默认网页标题和网站图标,引入elementui

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,31 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: '河南大学软件学院交流论坛',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '河南大学软件学院学习生活交流问答网站' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/bitbug_favicon.ico' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'element-ui/lib/theme-chalk/index.css',
+    'element-ui/lib/theme-chalk/display.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugin/vuePlugin/test.js'
+    '~/plugin/vuePlugin/test.js',
+    '~/plugin/element-plugin.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
